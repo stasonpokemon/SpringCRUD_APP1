ddbcd235097f61aa70ac0811eba51eb8c177b0ac
Stanislav Trebhikov <egorgus123@gmail.com>
2021-12-16 21:10:14 +0300

    Initial commit

---
+++ src/main/java/com/dao/PersonDAO.java
@@ -0,0 +1,34 @@
+package com.dao;
+
+import com.pojo.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT = 0;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT, "Jack"));
+        people.add(new Person(++PEOPLE_COUNT, "Pol"));
+        people.add(new Person(++PEOPLE_COUNT, "Terry"));
+        people.add(new Person(++PEOPLE_COUNT, "Katy"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id){
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+
+    public void save(Person person) {
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
+    }
+}
